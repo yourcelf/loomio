35e93167d142e1539e09534c53c5388f0a5024f5
Richard Bartlett <rich@loomio.org>
2014-10-28 12:44:15 +1300

    fix group creator migration

--- db/migrate/20141027212154_populate_group_creator_with_progress.rb
+++ db/migrate/20141027212154_populate_group_creator_with_progress.rb
@@ -1,7 +1,11 @@
-class PopulateGroupCreator < ActiveRecord::Migration
+class PopulateGroupCreatorWithProgress < ActiveRecord::Migration
   def change
     #for each group assign first admin with membership and assign as creator
     #could there be a Membership level of creator?
+    progress_bar = ProgressBar.create( format: "(\e[32m%c/%C\e[0m) %a |%B| \e[31m%e\e[0m ",
+                                       progress_mark: "\e[32m/\e[0m",
+                                       total: Group.count )
+
     Group.includes(:memberships).find_each(batch_size: 100) do |group|
       if admin = group.memberships.where(admin: true).first
         group.creator_id = admin.user_id
@@ -10,6 +14,7 @@ class PopulateGroupCreator < ActiveRecord::Migration
         group.creator_id = member.user_id
         group.save!
       end
+      progress_bar.increment
     end
   end
 end
