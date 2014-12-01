ab05556482b1bdaa94e48dc175c40f85b50ac3ce
Richard Bartlett <rich@loomio.org>
2014-12-01 14:30:59 +1300

    make loomio X work

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -1,7 +1,12 @@
 class MarketingController < ApplicationController
+   end
   def index
-    if stale?(1) # bump this when we change the frontpage
-      render layout: false
+    if current_user_or_visitor.is_logged_in?
+     redirect_to dashboard_path
+    elsif ENV['SHOW_LOOMIO_ORG_MARKETING']
+     render layout: false
+    else
+     redirect_to new_user_session_path
     end
   end
 end
